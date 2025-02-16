e81dc922b46ecac98d026f98781f7a3946fd1803
Daniel Azevedo <daniel.azevedo.dev@gmail.com>
2025-02-16 16:37:03 +0000

    fix test to skip database checks

--- src/test/java/dev/dazevedo/home/HomeApplicationTests.java
+++ src/test/java/dev/dazevedo/home/HomeApplicationTests.java
@@ -1,9 +1,13 @@
 package dev.dazevedo.home;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class HomeApplicationTests {
 
     @Test
