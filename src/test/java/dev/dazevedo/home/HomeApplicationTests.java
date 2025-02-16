e6ee5c2ad70be348acb8358f295d047c87d779e0
Daniel Azevedo <daniel.azevedo.dev@gmail.com>
2025-02-16 17:31:22 +0000

    fix application.properties to hide db password

--- src/test/java/dev/dazevedo/home/HomeApplicationTests.java
+++ src/test/java/dev/dazevedo/home/HomeApplicationTests.java
@@ -1,13 +1,9 @@
 package dev.dazevedo.home;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest()
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class HomeApplicationTests {
 
     @Test
